24b010ec9adda556e620e5affae755d60abe4bd3
David Gao <gao.ten@northeastern.edu>
2024-03-11 17:45:24 -0400

    go types learning

--- introduction/main.go
+++ introduction/main.go
@@ -3,6 +3,14 @@ import "fmt" // format
 
 // create function
 func main() {
-    // code
-    fmt.Println("Hello, World!") // print
+	// code
+	fmt.Println("Hello, World!") // print
+	var intNum int64 = 10        // variable
+	fmt.Println(intNum)          // print 10
+	var floatNum float64 = 10.5  // variable
+	fmt.Println(floatNum)        // print 10.5
+	// NOTE: string length are counted in ascii
+	fmt.Println(len("test")) // print 4
+	fmt.Println(len("A"))    // print 1
+	fmt.Println(len("γ"))    // print 2
 }
