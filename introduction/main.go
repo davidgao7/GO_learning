f22a38520442e52b8b4659c27e37cc9c672e7a59
David Gao <gao.ten@northeastern.edu>
2024-03-20 14:21:15 -0400

    go scanner str

--- introduction/main.go
+++ introduction/main.go
@@ -2,15 +2,22 @@ package main // special package name
 import "fmt" // format
 
 // create function
-func main() {
+func main() { // main package need main function
 	// code
-	fmt.Println("Hello, World!") // print
-	var intNum int64 = 10        // variable
-	fmt.Println(intNum)          // print 10
-	var floatNum float64 = 10.5  // variable
-	fmt.Println(floatNum)        // print 10.5
+	fmt.Println("Hello, World!")        // print
+	fmt.Printf("Hello, %s!\n", "World") // print with format
+	var intNum int64 = 10               // variable
+	fmt.Println(intNum)                 // print 10
+	var floatNum float64 = 10.5         // variable
+	fmt.Println(floatNum)               // print 10.5
 	// NOTE: string length are counted in ascii
 	fmt.Println(len("test")) // print 4
 	fmt.Println(len("A"))    // print 1
 	fmt.Println(len("γ"))    // print 2
+
+	// scan input
+	var appleNum string
+	fmt.Printf("number of apple?")
+	fmt.Scanln(&appleNum) // takes address of appleNum
+	fmt.Printf(appleNum)
 }
