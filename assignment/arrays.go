ad124bd9c0e281aeb4f5f8b55dbe2e0866f4a89d
David Gao <gao.ten@northeastern.edu>
2024-03-23 17:07:49 -0400

    declare an array, append new element, index, slice it

--- assignment/arrays.go
+++ assignment/arrays.go
@@ -1,17 +1,23 @@
 package main
 
 import (
-   "fmt" 
+	"fmt"
 )
 
-// array of 5 integers
-var x = [5] int {1, 2, 3, 4, 5} 
+func main() {
+	// array of 5 integers
+	var x = [5]int{1, 2, 3, 4, 5}
 
-for i, v range x {}
-   fmt.Println("index %d, val %d",  i, v)
+	for i, v := range x {
+		fmt.Printf("index %d, val %d\n", i, v)
+	}
+
+	// slices
+	// similar to arraylist in java
+	sli := make([]int, 5, 5)
+	fmt.Println(sli) // [0 0 0 0 0]
+	// the slice can automatically grow when you append
+	slice := append(sli, 6) // [0 0 0 0 0 6]
+	fmt.Println(slice)      // [0 0 0 0 0 6]
+
 }
-
-// slices
-// similar to arraylist in java
-sli = make([]int, 5, 5)
-fmt..println(sli) // [0 0 0 0 0]
